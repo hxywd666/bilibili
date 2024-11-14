f70c02fef1d87f1e9c0470edea840fdb966243d9
hlxllo <3337174805@qq.com>
2024-11-14 15:07:01 +0800

    feat:add file upload

--- bilibili-web/src/main/java/com/bilibili/BilibiliWebApplication.java
+++ bilibili-web/src/main/java/com/bilibili/BilibiliWebApplication.java
@@ -1,10 +1,13 @@
 package com.bilibili;
 
+import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+// 启用 x-file-storage
+@EnableFileStorage
 @SpringBootApplication
 @EnableAsync
 public class BilibiliWebApplication {
---
+++ bilibili-web/src/main/java/com/bilibili/controller/FileDetailController.java
@@ -0,0 +1,41 @@
+package com.bilibili.controller;
+
+import org.dromara.x.file.storage.core.FileInfo;
+import org.dromara.x.file.storage.core.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class FileDetailController {
+
+    @Autowired
+    private FileStorageService fileStorageService;//注入实列
+
+    /**
+     * 上传文件
+     */
+    @PostMapping("/upload")
+    public FileInfo upload(MultipartFile file) {
+        return fileStorageService.of(file).upload();
+    }
+    
+    /**
+     * 上传文件，成功返回文件 url
+     */
+    @PostMapping("/upload2")
+    public String upload2(MultipartFile file) {
+        FileInfo fileInfo = fileStorageService.of(file)
+                .setPath("upload/") //保存到相对路径下，为了方便管理，不需要可以不写
+                .setSaveFilename("image.jpg") //设置保存的文件名，不需要可以不写，会随机生成
+                .setObjectId("0")   //关联对象id，为了方便管理，不需要可以不写
+                .setObjectType("0") //关联对象类型，为了方便管理，不需要可以不写
+                .putAttr("role","admin") //保存一些属性，可以在切面、保存上传记录、自定义存储平台等地方获取使用，不需要可以不写
+                .upload();  //将文件上传到对应地方
+        return fileInfo == null ? "上传失败！" : fileInfo.getUrl();
+    }
+
+}
